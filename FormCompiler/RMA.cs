f0ba065a08dacfbcc92d5b7dc4af2ab01c428774
Tim Kopp <tim.kopp@cyber-balance.com>
2020-03-11 16:09:44 -0400

    refactored for multi datacalls

---
+++ FormCompiler/RMA.cs
@@ -0,0 +1,69 @@
+﻿using SOM.IO;
+using SOM.Procedures;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using SOM.Extentions;
+namespace FormCompiler
+{
+// Process_FRMVAL();
+ // GenDBScript();
+    class RMA
+    {
+      
+        public static void Main(string[] args)
+        {
+            Cache.Write(""); 
+            Console.ForegroundColor = ConsoleColor.Green; 
+            GenerateNewForms(); 
+            Cache.CacheEdit();
+
+        }
+        private static string GetFileName(string fname)
+        {
+            fname = fname.Replace("Q1", "Q2");
+            return fname;
+        }
+        private static void GenerateNewForms() 
+        {
+            string root = @"D:\dev\CyberScope\CyberScopeBranch\CSwebdev\code\CyberScope\RMA\2020";
+            string dest = @"C:\temp\templates\CIO\compiled";
+           
+            string content = "";
+            DirectoryInfo DI = new DirectoryInfo($"{root}");  
+            foreach (var file in DI.GetFiles("2020_Q1_RMA_*", SearchOption.TopDirectoryOnly))
+            {
+                content = new FileReader(file.FullName).Read().ToString();
+                content = new SqlKeyValCompile(@"C:\temp\templates\CIO\KVScriptForm.sql").Execute(content);
+          
+                string newfile = $"{dest}\\{GetFileName(file.Name)}";
+                FileWriter fw = new FileWriter(newfile);
+                fw.Write(content);
+                 
+                content = new FileReader(newfile).Read().ToString();
+                content = Utils.PK_QuestionInject(content);
+                content = Utils.PKGroupInject(content);
+                content = Utils.PK_KeyInject(content);
+
+
+                if (newfile.EndsWith(".aspx")) {
+                    content = content.RemoveEmptyLines();
+                }
+               
+                fw = new FileWriter(newfile);
+                fw.Write(content); 
+            } 
+        }
+
+       
+    } 
+}
+
+ 
+
---
+++ FormCompiler/Utils.cs
@@ -0,0 +1,132 @@
+﻿using SOM.Procedures;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FormCompiler
+{
+    public static class Utils
+    {
+        public static string prefix = "\n\t\t\t";
+        public static Dictionary<string, string> FoundKeys = new Dictionary<string, string>();
+
+        public static string PK_QuestionInject(string content)
+        {
+            MatchCollection matches = Regex.Matches(content, "PK_Question=\"\\d{5}");
+            foreach (Match match in matches)
+            {
+                foreach (Capture capture in match.Captures)
+                {
+                    string PK = capture.Value.Replace("\"", "").Replace("PK_Question=", "");
+                    string target = new BlockExtractor("qpk", capture.Value, "<tr", "/tr>").Execute(content);
+                    if (target != "" && !FoundKeys.ContainsKey(PK))
+                    {
+                        FoundKeys.Add(PK, capture.Index.ToString());
+                        content = content.Replace(target, string.Format("{0}{2}{1}\n", Utils.QuestionInfo(PK), target, Utils.prefix));
+                    }
+                }
+            }
+            return content;
+        }
+        public static string PKGroupInject(string content)
+        {
+            MatchCollection matches = Regex.Matches(content, "Group\" PK_key=\"\\d{4,5}");
+            foreach (Match match in matches)
+            {
+                foreach (Capture capture in match.Captures)
+                {
+                    string PK = capture.Value.Replace("\"", "").Replace("Group PK_key=", "");
+                    string target = new BlockExtractor("qgpk", capture.Value, "<tr", "/tr>").Execute(content);
+                    if (target != "" && !FoundKeys.ContainsKey(PK))
+                    {
+                        FoundKeys.Add(PK, capture.Index.ToString());
+                        content = content.Replace(target, string.Format("{0}{2}{1}\n", Utils.GroupInfo(PK), target, Utils.prefix));
+                    }
+                }
+            }
+            return content;
+        }
+
+        public static string PK_KeyInject(string content)
+        {
+            MatchCollection matches = Regex.Matches(content, " PK_key=\"\\d{5}");
+            foreach (Match match in matches)
+            {
+                foreach (Capture capture in match.Captures)
+                {
+                    string PK = capture.Value.Replace("\"", "").Replace(" PK_key=", "");
+                    string target = new BlockExtractor("pk", capture.Value, "<tr", "/tr>").Execute(content);
+                    if (target != "" && !FoundKeys.ContainsKey(PK))
+                    {
+                        FoundKeys.Add(PK, capture.Index.ToString());
+                        content = content.Replace(target, string.Format("{0}{2}{1}\n", Utils.QuestionInfo(PK), target, Utils.prefix));
+                    }
+                }
+            }
+            return content;
+        }
+
+        
+        public static string GroupInfo(string PK_QuestionGroup)
+        {
+            StringBuilder SB = new StringBuilder();
+            if (PK_QuestionGroup == "<!--null-->")
+                return "";
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["CAClientConnectionString"].ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand($"SELECT TOP 1 * FROM fsma_QuestionGroups WHERE PK_QuestionGroup={PK_QuestionGroup}", conn))
+                {
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        if (rdr.Read())
+                        {
+                            SB.Append(Utils.prefix + "{");
+                            SB.AppendFormat("{1}\"PK_QuestionGroup\":\"{0}\",", rdr["PK_QuestionGroup"].ToString(), Utils.prefix);
+                            SB.AppendFormat("{1}\"GroupName\":\"{0}\",", rdr["GroupName"].ToString(), Utils.prefix);
+                            SB.AppendFormat("{1}\"PK_Form\":\"{0}\",", rdr["PK_Form"].ToString(), Utils.prefix);
+                            SB.AppendFormat("{1}\"FK_FormPage\":\"{0}\",", rdr["FK_FormPage"].ToString().Replace("\"", "'"), Utils.prefix);
+                            SB.AppendFormat("{1}\"Text\":\"{0}\"", rdr["Text"].ToString().Replace("\"", "'"), Utils.prefix);
+                            SB.Append(Utils.prefix + "}\n");
+                        }
+                    }
+                }
+            }
+            return string.Format("<!--fsma_QuestionGroup{1}{0}{1}-->", SB.ToString().Trim(), Utils.prefix);
+        }
+
+        public  static string QuestionInfo(string PK_Question)
+        {
+            StringBuilder SB = new StringBuilder();
+            if (PK_Question == "<!--null-->")
+                return "";
+            Console.Write($"{PK_Question} , ");
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["CAClientConnectionString"].ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand($"SELECT TOP 1 * FROM fsma_Questions WHERE PK_Question={PK_Question}", conn))
+                {
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        if (rdr.Read())
+                        {
+                            SB.Append(Utils.prefix + "{");
+                            SB.AppendFormat("{1}\"PK_Question\":\"{0}\",", rdr["PK_Question"].ToString(), Utils.prefix);
+                            SB.AppendFormat("{1}\"identifier_text\":\"{0}\",", rdr["identifier_text"].ToString(), Utils.prefix);
+                            SB.AppendFormat("{1}\"FK_QuestionGroup\":\"{0}\",", rdr["FK_QuestionGroup"].ToString(), Utils.prefix);
+                            SB.AppendFormat("{1}\"QuestionText\":\"{0}\",", rdr["QuestionText"].ToString().Replace("\"", "'"), Utils.prefix);
+                            SB.AppendFormat("{1}\"FK_QuestionType\":\"{0}\"", rdr["FK_QuestionType"].ToString(), Utils.prefix);
+                            SB.Append(Utils.prefix + "}\n");
+                        }
+                    }
+                }
+            }
+            return string.Format("<!--fsma_Question{1}{0}{1}-->", SB.ToString().Trim(), Utils.prefix);
+        }
+    }
+}
