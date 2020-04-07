c314897af874d8c63fd253932008c2b5e23e9c10
Tim Kopp <tim.kopp@cyber-balance.com>
2020-04-07 18:31:33 -0400

    refactor

---
+++ FormCompiler/Formatters/PropFormatter.cs
@@ -0,0 +1,50 @@
+﻿using SOM.Procedures.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Formatters
+{
+    public class PropFormatter
+    {
+    }
+    public class ccCSHARPModel : IPropFormatter
+    {
+        public string Format(PropDefinition propDef)
+        {
+            string format = "public {1} {0} {{ get; set; }}";
+            if (propDef.DATA_TYPE.ToLower().Contains("int"))
+                return string.Format(format, propDef.NAME, "int");
+            if (propDef.DATA_TYPE.ToLower().Contains("date"))
+                return string.Format(format, propDef.NAME, "DateTime");
+            return string.Format(format, propDef.NAME, "string");
+        }
+    }
+    public class ccNgInput : IPropFormatter
+    {
+        public string Format(PropDefinition propDef)
+        {
+            string format = "<input type=\"text\" id=\"{0}\" formControlName =\"{0}\" class=\"form-control\" />";
+
+            if (propDef.DATA_TYPE.ToLower().Contains("int"))
+            {
+                return string.Format(format, propDef.NAME, "int");
+            }
+            if (propDef.DATA_TYPE.ToLower().Contains("date"))
+                return string.Format(format, propDef.NAME, "DateTime");
+            return string.Format(format, propDef.NAME, "string");
+        }
+    }
+    public class ccNGInterface : IPropFormatter
+    {
+        public string Format(PropDefinition propDef)
+        {
+            string format = "{0}: {1};";
+            if (propDef.DATA_TYPE.ToLower().Contains("int"))
+                return string.Format(format, propDef.NAME, "number");
+            return string.Format(format, propDef.NAME, "string");
+        }
+    }
+}
--- FormCompiler/Program.cs
+++ FormCompiler/Program.cs
@@ -25,70 +25,23 @@ namespace Compiler
             
             FileReader fr = new FileReader($"{AppSettings.SourceDir}\\_class.txt");   
             Type type = Type.GetType("Compiler.Models.TemplateFile, Compiler");
+
+            //Func<PropDefinition, string> converter = (propdef) => (string.Format("this.{0} == form.{0};\n", propdef.NAME));
+            Func<PropDefinition, string> format = (propdef) =>
+            (
+                 propdef.DATA_TYPE.Contains("int")  ? "1" : "2"  
+                //string.Format("this.{0} == form.{0};\n", propdef.NAME)
+            );
              
-            Func<PropDefinition, string> converter = (propdef) => (string.Format("this.{0} == form.{0};\n", propdef.NAME));
-            Func<string, string, string> integrator = (c, r) => c.Replace("[fields]", r);
-            //NgFormFromModel
-            //NgInterfaceFromModel
-            IProcedure p = new TypeModelCompiler(
+            TypeModelCompiler p = new TypeModelCompiler(
                             "Compiler.Models.Invoice, Compiler",
-                            new ccNgInput(),
-                            (c, r) => c.Replace("[fields]", r).Replace("/>", "/>\n")
+                            new Propformatter(format),
+                            (c, r) => c.Replace("[model]", r) 
                         );
             string result = p.Execute(fr.Read());
             FileWriter w = new FileWriter($"{AppSettings.SourceDir}\\class.txt");
             w.Write($"{result}", true);
-            CompilationBuilder<AppSettingsCompiler> compiler = new CompilationBuilder<AppSettingsCompiler>();
-
-            // CompilationBuilder<AppSettingsCompiler> compiler = new CompilationBuilder<AppSettingsCompiler>();
-            // compiler.Init()
-            //     .CompileMode(CompileMode.Commit) 
-            //     .FileFilter("*_class.txt")
-            //     .ContentCompilation(
-            //         new List<IProcedure> {
-            //             new TableModelCompiler(
-            //                 "fsma_Questions",
-            //                 new ccCustomConverter(converter),
-            //                 (c,r) => c.Replace("[fields]", r)
-            //             ),
-            //             new JsonCompile("{\"[entity]\":\"Question\"}")
-            //         }
-            //     )
-            //     .FilenameCompilation(
-            //             new List<IProcedure> { new JsonCompile("{\"_in\":\"_question\"}") }
-            //     )
-            //     .Compile() ;
-
-            //
-            //    TemplateWriter TB = new TemplateWriter(
-            //        new List<TemplateFile>(){
-            //            new TemplateFile(){ Path=@"c:\temp\$testing\1\test.html", Content="[ind]", ContentCompilers=procs }
-            //        } 
-            //    );
-            //    TB.CreateDirectories=true;
-            //    TB.Build();
-            //
-
-            //CompilationBuilder<DirectoryRecompiler> compiler = new CompilationBuilder<DirectoryRecompiler>();
-            //IProcedure proc = new RepeaterCompile(
-
-            //   compiler.Init()
-            //       .CompileMode(CompileMode.Commit)
-            //       .Source("c:\\temp\\testing\\") 
-            //       .Dest("c:\\temp\\$testing\\", true) 
-            //       .ContentCompilation(
-            //           new List<IProcedure> { 
-            //           new JsonReplace("{\"1\":\"2\"}")
-            //           }
-            //       )
-            //       .FilenameCompilation(
-            //           new List<IProcedure> {
-            //           new JsonReplace("{\".txt\":\".doc\"}"),
-            //           new JsonReplace("{\"1\":\"2\"}")
-            //           }
-            //       )
-            //       .Compile(); 
-            //
+                   
         }
     } 
 }
